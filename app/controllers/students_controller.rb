fea0ed344983c19beb632846d75142b00f9f29c0
MARTINA_WEIDENBAUM <martinaweidenbaum1@MARTINA-WEIDENBAUMs-MacBook-Pro.local>
2017-08-12 13:36:19 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,6 +6,18 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @student = Student.find(params[:id])
+  end
+
+  def activate
+    # this updates the model to be the opposite 'active' boolean
+    @student = Student.find(params[:id])
+    if !@student.active
+      @student.update(active: true)
+    elsif @student.active
+        @student.update(active: false)
+    end
+    redirect_to student_path(@student)
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.create(first_name: "Peggy", last_name: "Sawyer", created_at: Time.now)
+
+Student.create(first_name: "Emily", last_name: "Michelle", active: true)
